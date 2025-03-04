c27f1b7a379f25eb22679449b95cd3379cb6ed0d
Jackiiez <u2440308@rts-365.dk>
2025-03-04 13:20:42 +0100

    .

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -17,13 +17,13 @@ function App() {
   return (
     <>
          <header>
-                    <img src="public/hifi_logo.svg" alt="Overlay Image" className="overlay-image" />
+                    <img src="hifi_logo.svg" alt="Overlay Image" className="overlay-image" />
                     <SmallNavBar />
                     <NavBar />
                 </header>
                 <main>
                     <div className="wave-img">
-                        <img src="public/linje.jpg" alt="" />
+                        <img src="linje.jpg" alt="line" />
                         <p className="overlay-text">OUR EDINBURGH SHOWROOM HAS NOW MOVED TO STUNNING NEW PREMISES ON JOPPA ROAD</p>
                     </div>
                 
