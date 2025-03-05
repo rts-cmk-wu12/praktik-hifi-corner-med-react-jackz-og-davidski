e92c5c10eea0a9234b70628ff719232d67fedfbe
Davidski7 <U2486889@rts-365.dk>
2025-03-05 11:33:58 +0100

    Jeg er ved at designe Category Siden

--- src/pages/hifi-shop-category-list.jsx
+++ src/pages/hifi-shop-category-list.jsx
@@ -1,9 +1,16 @@
 import HifiShopList from "../componets/hifi-shop-list";
+import "../style/shop-category-list.scss";
+import CategoryNavbar from "../componets/category-navbar";
+import NavBar from "../componets/navbar"
 
 function HifiShopCategoryList() {
     return (
         <div>
-            <HifiShopList />
+            <CategoryNavbar />
+            <NavBar />
+            <main>
+                <HifiShopList />
+            </main>
         </div>
     );
 }
