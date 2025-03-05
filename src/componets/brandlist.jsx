f5492d8f5b67f2eb004139c0ab29d04305c0dfd6
Jackiiez <u2440308@rts-365.dk>
2025-03-05 10:05:56 +0100

    brand list 99% færdig

--- src/componets/brandlist.jsx
+++ src/componets/brandlist.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import "../style/brandlist.scss";
+import { FaLocationDot, FaMagnifyingGlass } from 'react-icons/fa6';
+import { FaPhone, FaShoppingCart, FaUser } from 'react-icons/fa';
+import { IoMdMail } from 'react-icons/io';
+
 const brands = [
     { src: "brandlist_logo/arcam_logo.svg", alt: "arcam_logo" },
     { src: "brandlist_logo/audiolab.svg", alt: "audiolab_logo" },
@@ -22,50 +26,93 @@ const brands = [
     { src: "brandlist_logo/yamaha.svg", alt: "yamaha" },
 ];
 
-const BrandList = () => (
-    <div className="background">
-        <div className="backdrop">
-       
+const BrandList = () => {
+    return (
+        <div className="background">
+            <div className="backdrop">
 
-            <main id="main" className="main">
-                <div className="main_wrapper">
-                    <div className="main_top">
-                        <h1 className="main_top_header">TOP BRANDS</h1>
-                        <img src="hifi_logo.svg" alt="hifi_logo" className="main_top_image--hifi_logo" />
+                <div className="header__top">
+                    <img src="/hifilogo.jpg" alt="logo" />
+                    <div className="header_div">
+                        <form action="newsletter.html" method="get">
+                            <label className="form__label" htmlFor="email"></label>
+                            <input
+                                className="navform"
+                                type="text"
+                                id="email"
+                                name="search"
+                                placeholder="Search entire store here"
+                            />  <FaMagnifyingGlass
+                                className="magnify header_div_items" />
+                          
+                        </form>
+                        <FaUser className="fa-solid fa-user"/>
+                        <a className="header_div_items" href="">account</a>
+                        <FaShoppingCart  className="fa-solid fa-cart-shopping"/>
+                        <a className="header_div_items" href="cart.html">cart</a>
+                      <FaLocationDot  className="fa-solid fa-location-dot header_div_items"/>
+                      <FaPhone  className="fa-solid fa-location-dot header_div_items"/>
+                      <IoMdMail   className="fa-solid fa-location-dot header_div_items"/>
                     </div>
-                    <div className="main_bottom">
-                        <div>
-                            <div className="main_bottom_container-white">
-                                <div className="main_bottom_container-gradiant">
-                                    <div className="main_bottom_container-width">
-                                        <p>Here are just a few of the brands we stock:</p>
-                                        {brands.map((brand, index) => (
-                                            <img key={index} src={brand.src} alt={brand.alt} className={`logo ${brand.className || ''}`} />
-                                        ))}
+                </div>
+                <nav>
+                    <ul className="nav_bar">
+                        <li><a className="nav_bar_item" href="Forside.html">home</a></li>
+                        <li><a className="nav_bar_item" href="">about us</a></li>
+                        <li><a className="nav_bar_item highlight" href="brandlist.html">brands</a></li>
+                        <li><a className="nav_bar_item" href="">blog</a></li>
+                        <li><a className="nav_bar_item" href="">events</a></li>
+                        <li><a className="nav_bar_item" href="shop-frontpage.html">shop</a></li>
+                        <li><a className="nav_bar_item" href="">contact us</a></li>
+                    </ul>
+                </nav>
+
+                <main id="main" className="main">
+                    <div className="main_wrapper">
+                        <div className="main_top">
+                            <h1 className="main_top_header">TOP BRANDS</h1>
+                            <img src="hifi_logo.svg" alt="hifi_logo" className="main_top_image--hifi_logo" />
+                        </div>
+                        <div className="main_bottom">
+                            <div>
+                                <div className="main_bottom_container-white">
+                                    <div className="main_bottom_container-gradiant">
+                                        <div className="main_bottom_container-width">
+                                            <p>Here are just a few of the brands we stock:</p>
+                                            {brands.map((brand, index) => (
+                                                <img
+                                                    key={index}
+                                                    src={brand.src}
+                                                    alt={brand.alt}
+                                                    className={`logo ${brand.className || ''}`}
+                                                />
+                                            ))}
+                                        </div>
                                     </div>
-                                </div>
-                                <div className="main_bottom_container-photo">
-                                    <h2>Brands</h2>
-                                    <p>
-                                        As an it-dependent retailer of home audio and visual, we are able to select the very best
-                                        brands and manufacturers from across the world.<br /><br />
-                                        Not only do we have the best-known brands such as B&W, Devialet, KEF, Naim, and Linn; we also
-                                        stock and are well acquainted with smaller companies like Michell, Neat
-                                        Acoustics, Harbeth, SMF, Trichord Research, and Primaluna to name a few.<br /><br />
-                                        With over a hundred brands, we can happily say that if a brand meets our high standards for
-                                        sound reproduction, then you can be assured to find it at Hi-Fi Corner.
-                                    </p>
-                                    <a href="shop-frontpage.html">VISIT OUR SHOP TO FIND MORE BRANDS</a>
+                                    <div className="main_bottom_container-photo">
+                                        <h2>Brands</h2>
+                                        <p>
+                                            As an it-dependent retailer of home audio and visual, we are able to select the very best
+                                            brands and manufacturers from across the world.<br /><br />
+                                            Not only do we have the best-known brands such as B&W, Devialet, KEF, Naim, and Linn; we also
+                                            stock and are well acquainted with smaller companies like Michell, Neat
+                                            Acoustics, Harbeth, SMF, Trichord Research, and Primaluna to name a few.<br /><br />
+                                            With over a hundred brands, we can happily say that if a brand meets our high standards for
+                                            sound reproduction, then you can be assured to find it at Hi-Fi Corner.
+                                        </p>
+                                        <a href="shop-frontpage.html">VISIT OUR SHOP TO FIND MORE BRANDS</a>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                </div>
-            </main>
+                </main>
+            </div>
         </div>
-    </div>
-);
+    );
+};
 
 export default BrandList;
+
 
 
--- src/pages/brand-list.jsx
+++ src/pages/brand-list.jsx
@@ -1,7 +1,7 @@
 import Brandlist from "../componets/brandlist"
 import Footer from "../componets/footer"
-import NavBar from "../componets/navbar"
-import SmallNavBar from "../componets/smallnavbar"
+
+
 
 
 
@@ -16,7 +16,9 @@ function BrandList() {
     return (
         <>
 
-         
+ 
+           
+            
             <main>
                 <Brandlist />
             </main>
