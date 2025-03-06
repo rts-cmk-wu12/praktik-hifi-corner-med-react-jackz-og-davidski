d9ec268ec5a53c0af57e5f0b8577869175af9c8c
Jackiiez <u2440308@rts-365.dk>
2025-03-06 13:56:46 +0100

    details update

--- src/componets/detailspage.jsx
+++ src/componets/detailspage.jsx
@@ -1,20 +1,20 @@
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
 import "../style/details.scss";
 
 export default function DetailsAgents() {
-    const { id } = useParams(); 
+    const { id } = useParams();
     const [product, setProduct] = useState(null);
 
     useEffect(() => {
-        fetch(`/db.json`) 
+        fetch(`/db.json`)
             .then((response) => response.json())
             .then((data) => {
                 const allProducts = Object.values(data).flat();
                 const foundProduct = allProducts.find(item => item.id === parseInt(id));
 
                 if (foundProduct) {
-                    setProduct(foundProduct); 
+                    setProduct(foundProduct);
                 } else {
                     console.error("Product not found");
                 }
@@ -23,21 +23,26 @@ export default function DetailsAgents() {
     }, [id]);
 
     if (!product) {
-        return <p>Loading product details...</p>; 
+        return <p>Loading product details...</p>;
     }
 
     return (
         <div className="hundred">
-
+            <div class="product-category-location" id="product-category-location">
+            <Link to="/"className="shop__frontpage__gallery__section__link"><p>HOME /</p></Link>
+            <Link to={`/hifi-shop-category-list?category/${product.category}`}className="shop__frontpage__gallery__section__link"><p>HOME /</p></Link>
+            <Link to={product.link}className="shop__frontpage__gallery__section__link">HOME /</Link>
+            </div>
+            <img src="/sale_img.png" alt="sale" class="product_category_commersial-img"></img>
             <section className="details__section">
-            <ul>
-                <li key={product.id}>
-                    <img src={`/${product.photo}`} alt={product.name} />
-                    <h2>{product.name}</h2>
-                    <p>Price: {product.price} DKK</p>
-                    <p>Brand: {product.manufacturer}</p>
-                </li>
-            </ul></section>
+                <ul>
+                    <li key={product.id}>
+                        <img src={`/${product.photo}`} alt={product.name} />
+                        <h2>{product.name}</h2>
+                        <p>Price: {product.price} DKK</p>
+                        <p>Brand: {product.manufacturer}</p>
+                    </li>
+                </ul></section>
         </div>
     );
 }
@@ -47,4 +52,3 @@ export default function DetailsAgents() {
 
 
 
-    
--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -1,9 +1,17 @@
+import CategoryNavbar from "../componets/category-navbar";
 import Detailspage from "../componets/detailspage";
+import Footer from "../componets/footer";
+import NavBar from "../componets/navbar";
 
 
 function Details() {
     return (
+        <>
+         <CategoryNavbar />
+                    <NavBar />
      <Detailspage/>
+      <Footer />
+     </>
     );
 }
 
